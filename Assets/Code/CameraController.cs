13e830e33f0c054ff642d7aad0f60c77e73190b5
igarciava <igarciava@edu.tecnocampus.cat>
2022-11-17 22:00:39 +0100

    sexo

---
+++ Assets/Code/CameraController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform LookAtTransform;
+    public float MinDistance = 3.0f;
+    public float MaxDistance = 15.0f;
+    public float YawRotationalSpeed = 720.0f;
+    public float PitchRotationalSpeed = 360.0f;
+
+    float Pitch = 0.0f;
+    public float MinPitch = -60.0f;
+    public float MaxPitch = 20.0f;
+
+    [Header("AvoidObjects")]
+    public LayerMask AvoidObjectsLayerMask;
+    public float AvoidObjectsOffset= 0.1f;
+
+    [Header("Debug")]
+    public KeyCode m_DebugLockAngleKeyCode = KeyCode.I;
+    bool m_AngleLocked = false;
+    bool m_AimLocked = true;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        m_AimLocked = Cursor.lockState == CursorLockMode.Locked;
+    }
+
+#if UNITY_EDITOR
+    void UpdateInputDebug()
+    {
+        if (Input.GetKeyDown(m_DebugLockAngleKeyCode))
+        {
+            m_AngleLocked = !m_AngleLocked;
+            if (Cursor.lockState == CursorLockMode.Locked)
+                Cursor.lockState = CursorLockMode.None;
+            else
+                Cursor.lockState = CursorLockMode.Locked;
+            m_AimLocked = Cursor.lockState == CursorLockMode.Locked;
+        }
+    }
+#endif
+
+    private void LateUpdate()
+    {
+#if UNITY_EDITOR
+        UpdateInputDebug();
+#endif
+
+        float l_MouseX = Input.GetAxis("Mouse X");
+        float l_MouseY = Input.GetAxis("Mouse Y");
+#if UNITY_EDITOR
+        if(m_AngleLocked)
+        {
+            l_MouseX = 0.0f;
+            l_MouseY = 0.0f;
+        }
+#endif
+        transform.LookAt(LookAtTransform.position);
+        float l_Distance = Vector3.Distance(transform.position, LookAtTransform.position);
+        l_Distance = Mathf.Clamp(l_Distance, MinDistance, MaxDistance);
+        Vector3 l_EulerAngles = transform.rotation.eulerAngles;
+        float l_Yaw = l_EulerAngles.y;
+
+        l_Yaw += l_MouseX * YawRotationalSpeed * Time.deltaTime;
+        Pitch += l_MouseY * PitchRotationalSpeed * Time.deltaTime;
+        Pitch = Mathf.Clamp(Pitch, MinPitch, MaxPitch);
+
+        Vector3 l_ForwardCamera = new Vector3(Mathf.Sin(l_Yaw * Mathf.Deg2Rad)* Mathf.Cos(Pitch * Mathf.Deg2Rad),
+            Mathf.Sin(Pitch * Mathf.Deg2Rad), Mathf.Cos(l_Yaw * Mathf.Deg2Rad) * Mathf.Cos(Pitch * Mathf.Deg2Rad));
+        Vector3 l_DesiredPosition = LookAtTransform.position - l_ForwardCamera * l_Distance;
+
+        Ray l_Ray = new Ray(LookAtTransform.position, -l_ForwardCamera);
+        RaycastHit l_RaycastHit;
+        if(Physics.Raycast(l_Ray, out l_RaycastHit, l_Distance, AvoidObjectsLayerMask.value))
+        {
+            l_DesiredPosition = l_RaycastHit.point + l_ForwardCamera * AvoidObjectsOffset;
+        }
+
+        transform.position = l_DesiredPosition;
+        transform.LookAt(LookAtTransform.position);
+    }
+}
---
+++ Assets/Code/MarioPlayerController.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+public class MarioPlayerController : MonoBehaviour
+{
+    public Animator Animator;
+    public CharacterController CharacterController;
+    
+    public Camera Camera;
+    public float LerpRotationPct = 0.3f;
+    public float WalkSpeed = 2.5f;
+    public float RunSpeed = 6.5f;
+    public float Gravity = -5.0f;
+
+    public float JumpSpeed = 10.0f;
+
+    private void Awake()
+    {
+        Animator = GetComponent<Animator>();
+        CharacterController = GetComponent<CharacterController>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float l_Speed = 0.0f;
+
+        Vector3 l_ForwardCamera = Camera.transform.forward;
+        Vector3 l_RightCamera = Camera.transform.right;
+        l_ForwardCamera.y = 0.0f;
+        l_RightCamera.y = 0.0f;
+        l_ForwardCamera.Normalize();
+        l_RightCamera.Normalize();
+        bool l_HasMovement = false;
+
+        Vector3 l_Movement = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            l_HasMovement = true; 
+            l_Movement = l_ForwardCamera;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            l_HasMovement = true;
+            l_Movement = -l_ForwardCamera;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            l_HasMovement = true;
+            l_Movement -= l_RightCamera;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            l_HasMovement = true;
+            l_Movement += l_RightCamera;
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Gravity = JumpSpeed;
+        }
+        l_Movement.Normalize();
+
+        float l_MovementSpeed = 0.0f;
+        if (l_HasMovement)
+        {
+            Quaternion l_LookRotation = Quaternion.LookRotation(l_Movement);
+            transform.rotation = Quaternion.Lerp(transform.rotation, l_LookRotation, LerpRotationPct);
+
+            l_Speed = 0.5f;
+            l_MovementSpeed = WalkSpeed;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                l_Speed = 1.0f;
+                l_MovementSpeed = RunSpeed;
+            }
+        }
+        Animator.SetFloat("Speed", l_Speed);
+        l_Movement = l_Movement * l_MovementSpeed * Time.deltaTime;
+        l_Movement.y = -1;
+        if (Input.GetMouseButtonDown(0))
+        {
+            Animator.SetTrigger("Punch");
+        }
+        CharacterController.Move(l_Movement);
+    }
+}
